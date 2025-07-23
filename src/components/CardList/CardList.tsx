15699ab1ffd303dbca3cedd113b2887f3ad8d14a
SunSundr <art.dizigner@gmail.com>
2025-07-23 19:12:40 +0400

    refactor: convert CardList to functional component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { BackdropSize, ImageConfiguration, PosterSize, TMDBVideo } from '~/types';
 import { getTransformSide } from '~/utils/getTransformSide';
 import { imageBaseUrl } from '~/utils/imageBaseUrl';
@@ -11,59 +11,50 @@ interface CardListProps {
   imagesConfig: ImageConfiguration;
 }
 
-interface CardListState {
-  backdropUrl: string;
-  posterUrl: string;
-  selectedVideo: TMDBVideo | null;
-  transformSide: string;
-}
+const CardList = ({ results, imagesConfig }: CardListProps) => {
+  const [backdropUrl] = useState(
+    imageBaseUrl({ size: BackdropSize.W780, type: 'backdrop' }, imagesConfig),
+  );
+  const [posterUrl] = useState(
+    imageBaseUrl({ size: PosterSize.W342, type: 'poster' }, imagesConfig),
+  );
+  const [selectedVideo, setSelectedVideo] = useState<TMDBVideo | null>(null);
+  const [transformSide, setTransformSide] = useState('');
 
-class CardList extends Component<CardListProps, CardListState> {
-  constructor(props: CardListProps) {
-    super(props);
-    this.state = {
-      backdropUrl: imageBaseUrl({ size: BackdropSize.W780, type: 'backdrop' }, props.imagesConfig),
-      posterUrl: imageBaseUrl({ size: PosterSize.W342, type: 'poster' }, props.imagesConfig),
-      selectedVideo: null,
-      transformSide: '',
-    };
-  }
-
-  handleCardClick = (video: TMDBVideo, event: React.MouseEvent) => {
-    this.setState({ selectedVideo: video, transformSide: getTransformSide(event) });
+  const handleCardClick = (video: TMDBVideo, event: React.MouseEvent) => {
+    setSelectedVideo(video);
+    setTransformSide(getTransformSide(event));
   };
 
-  handleCloseDetail = () => {
-    this.setState({ selectedVideo: null });
+  const handleCloseDetail = () => {
+    setSelectedVideo(null);
   };
 
-  render() {
-    return (
-      <>
-        <div className={styles.cardGrid}>
-          {this.props.results.map((item, index) => (
-            <Card
-              key={index}
-              index={index + 1}
-              video={item}
-              backdropUrl={this.state.backdropUrl}
-              posterUrl={this.state.posterUrl}
-              onClick={this.handleCardClick}
-            />
-          ))}
-        </div>
-        {this.state.selectedVideo && (
-          <Detail
-            video={this.state.selectedVideo}
-            backdropUrl={this.state.backdropUrl}
-            posterUrl={this.state.posterUrl}
-            onClose={this.handleCloseDetail}
-            transformSide={this.state.transformSide}
+  return (
+    <>
+      <div className={styles.cardGrid}>
+        {results.map((item, index) => (
+          <Card
+            key={index}
+            index={index + 1}
+            video={item}
+            backdropUrl={backdropUrl}
+            posterUrl={posterUrl}
+            onClick={handleCardClick}
           />
-        )}
-      </>
-    );
-  }
-}
+        ))}
+      </div>
+      {selectedVideo && (
+        <Detail
+          video={selectedVideo}
+          backdropUrl={backdropUrl}
+          posterUrl={posterUrl}
+          onClose={handleCloseDetail}
+          transformSide={transformSide}
+        />
+      )}
+    </>
+  );
+};
 
 export default CardList;
